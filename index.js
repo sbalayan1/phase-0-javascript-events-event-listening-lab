2ed154012f512ae2580cbb28ea2916924362a7bb
sbalayan1 <balayans2014@yahoo.com>
2021-04-30 15:53:20 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-function addingEventListener() {
+//declare new variable input that gets element by id input 
+const input = document.getElementById('input');
+
+//add 'click' event listener to input variable
+input.addEventListener('click',clickAlert);
+
+//create callback function 
+function clickAlert() {
+    alert('I was clicked');
 }
+
+function addingEventListener() {
+    input.addEventListener('click',clickAlert);
+}
+
+addingEventListener();

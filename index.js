02945add389120cc27cd62a5f2129a98896da8e1
sbalayan1 <balayans2014@yahoo.com>
2021-05-20 21:22:03 -0700

    commit tree 062fbce2e87e766c4e7e2aa9338c8155368490be

--- index.js
+++ index.js
@@ -2,15 +2,45 @@
 const input = document.getElementById('input');
 
 //add 'click' event listener to input variable
-input.addEventListener('click',clickAlert);
-
-//create callback function 
 function clickAlert() {
-    alert('I was clicked');
+    input.addEventListener('click', () => alert('I was clicked'))
 }
 
 function addingEventListener() {
-    input.addEventListener('click',clickAlert);
+    clickAlert();
 }
 
 addingEventListener();
+
+
+
+
+//add a new header with directions for what to do
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id','direction header')
+newHeader.textContent = 'Click on #5 for a neat response'
+
+document.querySelector('main').append(newHeader)
+
+
+
+//need to use an input tag to make it clickable? important note that you can create self-closing elements. 
+//not sure a div tag can do that which is why it's not working. 
+
+
+//create input element
+const input2 = document.createElement('input')
+input2.setAttribute('id','input2')
+input2.textContent = 'Here! Here! Click here!'
+
+document.querySelector('div div div div div').append(input2) //wonder why I can't append to the div5 variable? 
+
+
+
+//practice adding an event listener to nested <div>5</div> 
+const div5 = document.querySelector('div div div div div')
+
+const div5Event = div5.addEventListener('click', () => alert('Thanks for clicking me'))
+
+div5event();
